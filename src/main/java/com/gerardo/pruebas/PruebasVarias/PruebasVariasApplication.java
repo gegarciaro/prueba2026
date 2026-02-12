604716510d0b0abec8d06442492712a32b50fd81
Gerardo Garcia <gegarciaro@gmail.com>
2026-02-11 21:09:34 -0600

    Debio irse a QA?

--- src/main/java/com/gerardo/pruebas/PruebasVarias/PruebasVariasApplication.java
+++ src/main/java/com/gerardo/pruebas/PruebasVarias/PruebasVariasApplication.java
@@ -8,6 +8,8 @@ public class PruebasVariasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebasVariasApplication.class, args);
+
+        System.out.println("tengo esta version para QA donde salen letreros");
 	}
 
 }
